bfce463dbf80f9d271116f00a052c2c5da10e44c
Sam Kasyoki <samkasyoki@gmail.com>
2020-04-15 20:50:19 +0300

    connect to mongodb & redis

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,31 @@
 import express from "express";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import Redis from "ioredis";
+import mongoose from "mongoose";
 
-const app = express();
+import {
+  REDIS_OPTIONS,
+  SESSION_OPTIONS,
+  PORT,
+  MONGO_URI,
+  MONGO_OPTIONS,
+} from "./config";
 
-app.get("/", (_, res) => res.json({ message: "OK" }));
+(async () => {
+  await mongoose.connect(MONGO_URI, MONGO_OPTIONS);
 
-app.listen(3000, () => console.log("Server started on http://localhost:3000"));
+  const RedisStore = connectRedis(session);
+
+  const client = new Redis(REDIS_OPTIONS);
+
+  const app = express();
+
+  app.use(session({ ...SESSION_OPTIONS, store: new RedisStore({ client }) }));
+
+  app.get("/", (_, res) => res.json({ message: "OK" }));
+
+  app.listen(PORT, () =>
+    console.log(`Server started on http://localhost:${PORT}`)
+  );
+})();
